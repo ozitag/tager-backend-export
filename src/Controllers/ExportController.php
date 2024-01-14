a1c11a966c6109e80eb33ddf77d716f8f6e0af5a
Vital Ozierski <vital@ozerich.com>
2024-01-14 19:57:23 +0100

    add params to list

--- src/Controllers/ExportController.php
+++ src/Controllers/ExportController.php
@@ -12,6 +12,8 @@ use OZiTAG\Tager\Backend\Export\Models\ExportSession;
 use OZiTAG\Tager\Backend\Export\Operations\CreateExportOperation;
 use OZiTAG\Tager\Backend\Export\Repositories\ExportSessionRepository;
 use OZiTAG\Tager\Backend\Export\Requests\ExportStoreRequest;
+use OZiTAG\Tager\Backend\Export\TagerExport;
+use OZiTAG\Tager\Backend\Fields\Base\Field;
 
 class ExportController extends AdminCrudController
 {
@@ -60,6 +62,38 @@ class ExportController extends AdminCrudController
             'file' => function (ExportSession $exportSession) {
                 return $exportSession->file?->getShortJson();
             },
+            'params' => function(ExportSession $exportSession){
+                $params = $exportSession->params ? json_decode($exportSession->params, true) : [];
+                if(empty($params)){
+                    return [];
+                }
+
+                $strategy = TagerExport::getStrategy($exportSession->strategy);
+                $fields = $strategy ? $strategy->conditionalFields() : [];
+
+                $result = [];
+                foreach($params as $param => $value){
+                    if(empty($value)){
+                        continue;
+                    }
+
+                    /** @var Field $field */
+                   $field = $fields[$param] ?? null;
+
+                   $label = $field ? $field->getLabel() : $param;
+
+                   if(!$field){
+                       $result[$param] = is_array($value) ? implode(', ', $value) : $value;
+                   } else{
+                       $typeInstance = $field->getTypeInstance();
+                       $typeInstance->setValue($value);
+
+                       $result[$field->getLabel()] = $typeInstance->getLabelValue();
+                   }
+                }
+
+                return $result;
+            }
         ], true);
     }
 
